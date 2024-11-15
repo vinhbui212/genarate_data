de8073af115905ce1924e9ff47abfcf11d05dace
vinhbui212 <vinhhoc21@gmail.com>
2024-11-15 14:57:37 +0700

    qa! mq: `

--- src/main/java/org/example/feature2.java
+++ src/main/java/org/example/feature2.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class feature2 {
     public static void main(String[] args) {
-        System.out.println("feature2");
+        System.out.println("feature2d s");
     }
 }
